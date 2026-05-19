0d73fd0dfcfd1331b80349b0124608762b045d8e
melanmoulan0-gif <melanmoulan0@gmail.com>
2026-05-19 23:33:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function click1(){
+  let userAnswer=
+document.getElementById("kitten1").value;
+if (userAnswer=="Gravity"||userAnswer=="gravity"){
+  document.getElementById("answers").innerHTML="correct";}
+
+else { document.getElementById("answers").innerHTML="wrong";}
+}
+
+function click2(){
+  let userAnswer=
+document.getElementById("kitten2").value;
+if (userAnswer=="roots"||userAnswer=="Roots"){
+  document.getElementById("answers").innerHTML="correct";}
+
+else { document.getElementById("answers").innerHTML="wrong";}
+}
+
+function click3(){
+  let userAnswer=
+document.getElementById("kitten3").value;
+if (userAnswer=="sodium chloride"||userAnswer=="Sodium Chloride"){
+  document.getElementById("answers").innerHTML="correct";}
+
+else { document.getElementById("answers").innerHTML="wrong";}
+}
